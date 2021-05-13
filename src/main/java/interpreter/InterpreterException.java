6215b823f6f37239dfe9aa5dd6b2137376285296
thibault.wafflard <thibault.wafflard@student.uclouvain.be>
2021-05-13 10:43:53 +0200

    InterpreterException

---
+++ src/main/java/interpreter/InterpreterException.java
@@ -0,0 +1,10 @@
+package interpreter;
+
+/**
+ * Simple wrapper for exceptions thrown while running the interpreter.
+ */
+public final class InterpreterException extends RuntimeException {
+    public InterpreterException (String message, Throwable cause) {
+        super(message, cause);
+    }
+}
